c7c02577f880fcab0ab6a92efa0cffa9eabc72b3
madhujatathe <madhutathe38@gmail.com>
2026-04-27 23:14:16 +0530

    programs

---
+++ maximum_of_3.py
@@ -0,0 +1,4 @@
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print("max =", max(a,b,c))
---
+++ simple_calculator.py
@@ -0,0 +1,3 @@
+a = int(input("enter first number: "))
+b = int(input("enter decond number: "))
+print ("sum =", a+b) 
